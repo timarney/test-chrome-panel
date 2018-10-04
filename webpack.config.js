8a93788e8d22a575b09b6f7e5070a9d1022a8617
Tim Arney <tim@line37.com>
2018-10-04 10:28:27 -0400

    config update

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,30 @@
 const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-module.exports = {
-  entry: "./src/panel.js",
-  mode: "development",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
-  },
-  plugins: [
-    new CopyWebpackPlugin([{ from: "src", to: "./", ignore: "panel.js" }])
-  ]
+module.exports = env => {
+  return {
+    entry: "./src/panel.js",
+    mode: env.mode,
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js"
+    },
+    plugins: [
+      new CopyWebpackPlugin([{ from: "src", to: "./", ignore: "panel.js" }])
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"]
+            }
+          }
+        }
+      ]
+    }
+  };
 };
